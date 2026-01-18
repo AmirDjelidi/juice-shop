d44797ba0df5ae98f04de0588f397fe617de3a23
Romain Billy <romain.billy@efrei.net>
2026-01-18 15:30:44 +0100

    fix products search logic

--- data/static/codefixes/unionSqlInjectionChallenge_1.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_1.ts
@@ -1,17 +1,37 @@
+import { QueryTypes } from 'sequelize'
+import {NextFunction} from "express";
+
 export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
+    console.log("here")
+
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    criteria.replace(/"|'|;|and|or/i, "")
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
+    criteria = criteria.toString().substring(0, 200)
+
+    // old blacklist removed – not reliable for security
+
+    models.sequelize.query(
+      `SELECT * FROM Products
+       WHERE (
+         (name LIKE :search OR description LIKE :search)
+         AND deletedAt IS NULL
+       )
+       ORDER BY name`,
+      {
+        replacements: { search: `%${criteria}%` },
+        type: QueryTypes.SELECT
+      }
+    )
+      .then((products: any[]) => {
+
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
         }
+
         res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
+      })
+      .catch((error: ErrorWithParent) => {
         next(error.parent)
       })
   }
--- data/static/codefixes/unionSqlInjectionChallenge_3.ts
+++ data/static/codefixes/unionSqlInjectionChallenge_3.ts
@@ -1,21 +1,46 @@
+import { QueryTypes } from 'sequelize'
+import {NextFunction} from "express";
+
 export function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
+
+    console.log("here")
+
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
-    criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    // only allow apple or orange related searches
-    if (!criteria.startsWith("apple") || !criteria.startsWith("orange")) {
+    criteria = criteria.toString().substring(0, 200)
+
+    // only allow apple OR orange searches ← fixed logic
+    if (
+      !criteria.startsWith("apple") &&
+      !criteria.startsWith("orange")
+    ) {
       res.status(400).send()
       return
     }
-    models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
-      .then(([products]: any) => {
-        const dataString = JSON.stringify(products)
+
+    // ---- SAFE: parameterized query ----
+    models.sequelize.query(
+      `SELECT * FROM Products
+       WHERE (
+               (name LIKE :search OR description LIKE :search)
+                 AND deletedAt IS NULL
+               )
+       ORDER BY name`,
+      {
+        replacements: { search: `%${criteria}%` },
+        type: QueryTypes.SELECT
+      }
+    )
+      .then((products: any[]) => {
+
         for (let i = 0; i < products.length; i++) {
           products[i].name = req.__(products[i].name)
           products[i].description = req.__(products[i].description)
         }
+
         res.json(utils.queryResultToJson(products))
-      }).catch((error: ErrorWithParent) => {
+      })
+      .catch((error: ErrorWithParent) => {
         next(error.parent)
       })
   }
